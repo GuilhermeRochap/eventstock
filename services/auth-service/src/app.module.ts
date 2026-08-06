c52cb0a0e44dd1c19a98e3739affb47b74e330d0
Guilherme Rocha <guirpessanha@gmail.com>
2026-08-06 00:07:44 -0300

    feat(auth-service): endpoint de login com JWT (access + refresh token)

--- services/auth-service/src/app.module.ts
+++ services/auth-service/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { SupabaseModule } from './supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
 import { CompaniesModule } from './companies/companies.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     SupabaseModule,
     CompaniesModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
